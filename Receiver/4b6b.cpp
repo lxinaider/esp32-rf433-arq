16e29ee79a588c167670345002bc1d12e674094a
Luiz Henrique <moeefa@protonmail.com>
2026-06-16 16:18:11 -0400

    fix: crc check with remainder

--- Receiver/4b6b.cpp
+++ Receiver/4b6b.cpp
@@ -1,14 +1,16 @@
 #include "4b6b.h"
 
 uint8_t dec4b6b(uint8_t symbol) {
+  // Busca linear na tabela
   for (uint8_t i = 0; i < 16; i++)
     if (encode_table[i] == symbol)
       return i;
 
-  return 0xFF; // símbolo inválido
+  return 0xFF; // Símbolo não encontrado
 }
 
 void enc4b6b(uint8_t byte, uint8_t &hi, uint8_t &lo) {
+  // Divide o byte em nibble alto e baixo, codifica cada um pela tabela.
   hi = encode_table[byte >> 4];
   lo = encode_table[byte & 0x0F];
 }
